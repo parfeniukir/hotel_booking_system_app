6978eced61aaba125782baa4c2c7911013b10fef
Ivanna Parfeniuk <parfeniuk.ivannaa@gmail.com>
2025-11-11 21:31:05 +0200

    Add functionality

--- main.py
+++ main.py
@@ -12,16 +12,35 @@
 
 import pandas as pd
 
+df_hotels = pd.read_csv("hotels.csv", dtype={"id": str})
+# print(df_hotels)
+# print(type(df_hotels))
+
 
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df_hotels.loc[df_hotels["id"] == self.hotel_id, "name"].squeeze()
+        # print("HOTEL NAME", self.name)
 
     def booking(self):
-        pass
+        df_hotels.loc[df_hotels["id"] == self.hotel_id, "available"] = "no"
+        df_hotels.to_csv("hotels.csv", index=False)
 
     def available(self):
-        pass
+        """Check if the hotel is available"""
+        # фільтрація хотелю по ID і отримання значення колонки available, по цьому IDa
+        available = df_hotels.loc[df_hotels["id"] == self.hotel_id, "available"].squeeze()
+        try:
+            if available == "yes":
+                return True
+            else:
+                return False
+        except ValueError:
+            print("This ID is not exist!")
+            exit()
+        # print(available)
+        # print(type(available))
 
 
 class ReservationTicket:
@@ -30,14 +49,19 @@ class ReservationTicket:
         self.hotel_obj = hotel_obj
 
     def generate(self):
-        pass
+        ticket = f"""
+        Thank you for your reservation.
+        Here is your booking data:
+        Name: {self.customer_name}.
+        Hotel name: {self.hotel_obj.name}
+        """
+        return ticket
 
 
 def main():
-    df = pd.read_csv("hotels.csv")
-    print(df)
+    print(df_hotels)
 
-    hotel_id = input("Enter the ID of the hotel")
+    hotel_id = input("Enter the ID of the hotel: ")
     hotel = Hotel(hotel_id=hotel_id)
 
     if hotel.available():
@@ -47,7 +71,7 @@ def main():
             customer_name=customer_name,
             hotel_obj=hotel,
         )
-        reservation_ticket.generate()
+        print(reservation_ticket.generate())
     else:
         print("Sorry, the hotel is not available for booking.")
 
